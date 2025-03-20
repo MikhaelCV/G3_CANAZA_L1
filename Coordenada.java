6e72b762d844f4852c0c3e1604c3d5571ea255e7
MikhaelCV <mikhael.canaza@estudiante.ucsm.edu.pe>
2025-03-20 11:23:17 -0500

    Update Coordenada.java

--- Coordenada.java
+++ Coordenada.java
@@ -1,49 +1,72 @@
-import java.util.*;
+public class Coordenada {
+	// Atributos de la clase Coordenada
+	private double x; // Coordenada en el eje X
+	private double y; // Coordenada en el eje Y
+	private Coordenada c; // Atributo de tipo Coordenada
 
-class Coordenada {
-    private double x;
-    private double y;
+	// Constructor por defecto, inicializa los atributos x, y a cero
+	public Coordenada( ){
+		x = 0;
+		y = 0;
+	}
 
-    public Coordenada() {
-        this.x = 0;
-        this.y = 0;
-    }
+	// Constructor con parámetros, inicializa los atributos x, y con los valores proporcionados
+	public Coordenada(double x, double y ){
+		this.x = x;
+		this.y = y;
+	}
 
-    public Coordenada(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
+	// Constructor con parámetro de tipo Coordenada, inicializa el atributo co con el valor proporcionado
+	public Coordenada(Coordenada c ){
+		this.c = c;
+	}
 
-    public Coordenada(Coordenada c) {
-        this.x = c.x;
-        this.y = c.y;
-    }
+	// Métodos setter para establecer los valores de x e y
+	void setx(int x) {
+		this.x = x;
+	}
+	void sety(int y){
+		this.y = y;
+	}
 
-    public void setX(double x) {
-        this.x = x;
-    }
+	// Métodos getter para obtener los valores de x e y
+	double getx(){
+	 return x;
+	}
+	double gety(){
+	 return y;
+	}
 
-    public void setY(double y) {
-        this.y = y;
-    }
+	// Método para el cálculo de la distancia euclideana entre esta Coordenada y otra proporcionada como parámetro
+	public double distancia(Coordenada c){
+		double Distx, Disty, distancia;
+	    Distx = c.getx();
+	    Disty = c.gety();
+	    distancia = Distx - Disty;
+	    if (distancia < 0){
+	          distancia = distancia * -1;
+	     }
+	     return distancia;
+	}
 
-    public double getX() {
-        return x;
-    }
+	// Método de clase que calcula la distancia euclideana entre dos Coordenadas proporcionadas como parámetros
+	public static double distancia(Coordenada c1, Coordenada c2){
+	    double Distx = 0,Disty = 0, distancia;
+	    Distx = c1.getx()-c2.getx();
+	    Distx = c1.gety()-c2.gety();
+	   // Obtener valores absolutos
+	   if(Distx < 0){
+	      Distx=(Distx * -1);
+	    }
+	   if(Disty < 0){
+	      Disty = (Distx * -1);
+	     }
+	     distancia=Math.sqrt(Distx * Distx + Disty * Disty);
+	      return distancia;
+	}
 
-    public double getY() {
-        return y;
-    }
-
-    public double distancia(Coordenada c) {
-        return Math.sqrt(Math.pow(this.x - c.x, 2) + Math.pow(this.y - c.y, 2));
-    }
-
-    public static double distancia(Coordenada c1, Coordenada c2) {
-        return c1.distancia(c2);
-    }
-
-    public String toString() {
-        return "(" + x + ", " + y + ")";
-    }
+	// Método toString para representar la Coordenada en forma de texto
+	public String toString(){
+		return"("+ x + ";" + y + ")";
+	}
 }
